4dc540355061ccb2145b4838f1c5d75d01811980
Paul <paul.j.millar.1971@gmail.com>
2016-09-17 17:01:42 +0100

    Added Anonymous Pipe Example

---
+++ AnonymousPipeExample/AnonymousPipe.Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace AnonymousPipe.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                using (PipeStream client = new AnonymousPipeClientStream(PipeDirection.In, args[0]))
+                {
+                    // Display trans mode
+                    Console.WriteLine($"[CLIENT] Current TransmissionMode: {client.TransmissionMode}");
+
+                    using (StreamReader reader = new StreamReader(client))
+                    {
+                        // Display the read text to the console
+                        string temp;
+
+                        // Wait for 'sync message' from the server.
+                        do
+                        {
+                            Console.WriteLine("[CLIENT] Wait for sync...");
+                            temp = reader.ReadLine();
+                        }
+                        while (!temp.StartsWith("SYNC"));
+
+                        Console.WriteLine($"[CLIENT] Echo: {temp}");
+                    }
+                }
+            }
+
+            Console.Write("[CLIENT] Press Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AnonymousPipeExample/AnonymousPipe.Server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+
+namespace AnonymousPipe.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Process for running the client
+            Process client = new Process();
+            client.StartInfo.FileName = "AnonymousPipe.Client.exe"; // path for our client executable
+
+            using (AnonymousPipeServerStream server = new AnonymousPipeServerStream(PipeDirection.Out, HandleInheritability.Inheritable))
+            {
+                // Display trans mode
+                Console.WriteLine($"[SERVER] Current TransmissionMode: {server.TransmissionMode}");
+
+                // Pass the client process a handle to the server
+                client.StartInfo.Arguments = server.GetClientHandleAsString();
+                client.StartInfo.UseShellExecute = false;
+                client.Start();
+
+                // free any resources
+                /*
+                 * The DisposeLocalCopyOfClientHandle method should be called after the client handle has been passed to the client. 
+                 * If this method is not called, the AnonymousPipeServerStream object will not receive notice when the client disposes 
+                 * of its PipeStream object.
+                 */
+                server.DisposeLocalCopyOfClientHandle();
+
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(server))
+                    {
+                        writer.AutoFlush = true;
+
+                        // send message
+                        writer.Write("SYNC");
+
+                        // wait for the other end of the pipe to read the message 
+                        server.WaitForPipeDrain();
+
+                        // prompt for some input to send to client
+                        Console.Write("[SERVER] Enter text: ");
+                        writer.WriteLine(Console.ReadLine());
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Thread.Sleep(60000);
+                client.WaitForExit();
+                client.Close();
+                Console.WriteLine("[SERVER] Done");
+            }
+        }
+    }
+}
